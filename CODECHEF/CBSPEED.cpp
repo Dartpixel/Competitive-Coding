b1106044d2109af24014f11cc30e152858e0dd49
Dartpixel <skartiksharma2004@gmail.com>
2022-10-15 21:37:22 +0530

    Updated

---
+++ CODECHEF/CBSPEED.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <math.h>
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a >= b)
+    {
+        cout << "NO" << endl;
+    }
+    else
+    {
+        cout << "YES" << endl;
+    }
+    return 0;
+}
