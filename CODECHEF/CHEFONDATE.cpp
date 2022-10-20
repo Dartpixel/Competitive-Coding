41e747b55f7304d6d66daec1cba2ebab216ee2ad
Dartpixel <skartiksharma2004@gmail.com>
2022-10-20 22:47:47 +0530

    Updated

---
+++ CODECHEF/CHEFONDATE.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if (a < b)
+        {
+            cout << "NO" << endl;
+        }
+        else
+        {
+            cout << "YES" << endl;
+        }
+    }
+    return 0;
+}
