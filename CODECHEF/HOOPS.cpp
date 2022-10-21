c4ced8fed6e259eb9b7dee40bbf252303f8911c6
Dartpixel <skartiksharma2004@gmail.com>
2022-10-21 21:34:13 +0530

    Updated

---
+++ CODECHEF/HOOPS.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testcase, n;
+    cin >> testcase;
+    while (testcase--)
+    {
+        cin >> n;
+        cout << (n + 1) / 2 << endl;
+    }
+    return 0;
+}
