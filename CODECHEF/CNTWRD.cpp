43a740de1843db36f02973feb9d9fa726a75187d
Dartpixel <skartiksharma2004@gmail.com>
2022-10-26 14:25:42 +0530

    Updated

---
+++ CODECHEF/CNTWRD.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a * b << endl;
+    }
+    return 0;
+}
