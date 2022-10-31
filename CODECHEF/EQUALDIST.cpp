83e0901ff2b38f98de721ce3e615533e271a54e9
Dartpixel <skartiksharma2004@gmail.com>
2022-10-31 10:58:41 +0530

    Updated

---
+++ CODECHEF/EQUALDIST.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int a, b;
+        cin >> a >> b;
+        if ((a + b) % 2 == 0)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
