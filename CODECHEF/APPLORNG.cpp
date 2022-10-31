1c10da2e46c69d896e2cb56b7934d56d34277f15
Dartpixel <skartiksharma2004@gmail.com>
2022-10-31 10:53:12 +0530

    Updated

---
+++ CODECHEF/APPLORNG.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, a, b;
+    cin >> x;
+    cin >> a >> b;
+    if (a + b <= x)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+    return 0;
+}
