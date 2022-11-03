f21f8277842dc0c096e9a4cc26e72158832c0cf9
Dartpixel <skartiksharma2004@gmail.com>
2022-11-03 22:58:11 +0530

    Updated

---
+++ CODECHEF/TFPAPER.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int testcase;
+	int a,b;
+	cin>>testcase;
+	while(testcase--)
+	{
+	    cin>>a>>b;
+	    cout<<a-b<<endl;
+	}
+	return 0;
+}
