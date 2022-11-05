ed5445839f52a649a1a05c825a9b05a158fb490f
Dartpixel <skartiksharma2004@gmail.com>
2022-11-05 20:24:11 +0530

    Updated

---
+++ CODECHEF/CCHOCOLATES.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int testcase;
+	cin>>testcase;
+	while(testcase--)
+	{
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    cout<<((x*5)+(y*10))/z<<endl;
+	}
+	return 0;
+}
