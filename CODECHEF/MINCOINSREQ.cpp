1a707ab430a7f65bf4964096d97629c4be1ac087
Dartpixel <skartiksharma2004@gmail.com>
2022-11-07 21:26:24 +0530

    Updated

---
+++ CODECHEF/MINCOINSREQ.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int n;
+        cin >> n;
+        cout << n % 10 << endl;
+    }
+    return 0;
+}
