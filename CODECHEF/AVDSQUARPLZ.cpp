d35f8647a47f496f2aaea71812426f9f166aab4f
Dartpixel <skartiksharma2004@gmail.com>
2022-11-08 21:24:36 +0530

    Updated

---
+++ CODECHEF/AVDSQUARPLZ.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        while (n)
+        {
+            cout << n-- << " ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
