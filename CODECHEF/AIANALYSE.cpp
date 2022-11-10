32e7ae3db9a5115584211aa7ff2895746d723afe
Dartpixel <skartiksharma2004@gmail.com>
2022-11-10 17:00:26 +0530

    Updated

---
+++ CODECHEF/AIANALYSE.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    if (n <= 1000)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+    return 0;
+}
