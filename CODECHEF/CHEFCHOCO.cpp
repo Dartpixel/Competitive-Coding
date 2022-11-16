dc67c5b7cf7d4391365c59d3cd6d174b22bcaff8
Dartpixel <skartiksharma2004@gmail.com>
2022-11-16 19:50:23 +0530

    Updated

---
+++ CODECHEF/CHEFCHOCO.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        cout << (a - b) * c << endl;
+    }
+    return 0;
+}
