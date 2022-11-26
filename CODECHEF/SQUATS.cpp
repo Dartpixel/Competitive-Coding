d38e10484b26d253483228955e94bab1c3d0982e
Dartpixel <skartiksharma2004@gmail.com>
2022-11-26 17:00:17 +0530

    Updated

---
+++ CODECHEF/SQUATS.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int x;
+        cin >> x;
+        cout << x * 15 << endl;
+    }
+    return 0;
+}
