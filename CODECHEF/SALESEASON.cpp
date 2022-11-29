2c4ab95b9a3a08dfb552683a8c423721af7eb2d8
Dartpixel <skartiksharma2004@gmail.com>
2022-11-29 17:29:47 +0530

    Updated

---
+++ CODECHEF/SALESEASON.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int x;
+        cin >> x;
+        if (x <= 100)
+        {
+            cout << x << endl;
+        }
+        else if (x > 100 && x <= 1000)
+        {
+            cout << x - 25 << endl;
+        }
+        else if (x > 1000 && x <= 5000)
+        {
+            cout << x - 100 << endl;
+        }
+        else
+        {
+            cout << x - 500 << endl;
+        }
+    }
+    return 0;
+}
