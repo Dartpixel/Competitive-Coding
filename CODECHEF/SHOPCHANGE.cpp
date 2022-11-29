b3db75d99052367889f79cbd0456ed190e61dbc4
Dartpixel <skartiksharma2004@gmail.com>
2022-11-29 17:28:46 +0530

    Updated

---
+++ CODECHEF/SHOPCHANGE.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int n;
+        cin >> n;
+        cout << 100 - n << endl;
+    }
+    return 0;
+}
