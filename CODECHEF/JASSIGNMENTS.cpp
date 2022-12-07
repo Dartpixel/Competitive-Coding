bfb4a93556e80956c3d925a1882dd5d4e9ed5820
Dartpixel <skartiksharma2004@gmail.com>
2022-12-07 20:38:18 +0530

    Updated

---
+++ CODECHEF/JASSIGNMENTS.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int testcase;
+	cin>>testcase;
+	while(testcase--)
+	{
+	    int n;
+	    cin>>n;
+	    if(n+3<=10)
+	    {
+	        cout<<"Yes"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"No"<<endl;
+	    }
+	}
+	return 0;
+}
