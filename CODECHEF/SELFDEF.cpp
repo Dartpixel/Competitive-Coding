21560c306496d75e9951edb4fa62f062975a4814
Dartpixel <skartiksharma2004@gmail.com>
2022-12-20 17:42:22 +0530

    updated

---
+++ CODECHEF/SELFDEF.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	int testcase;
+	cin>>testcase;
+	while(testcase--)
+	{
+	    int n;
+	    cin>>n;
+	    int arr[n];
+	    for(int i=0;i<n; i++)
+	    {
+	        cin>>arr[i];
+	    }
+	    int c=0;
+	    for(int i=0 ;i<n; i++)
+	    {
+	        if(arr[i]>=10 && arr[i]<=60)
+	        {
+	            c++;
+	        }
+	    }
+	    cout<<c<<endl;
+	}
+	return 0;
+}
