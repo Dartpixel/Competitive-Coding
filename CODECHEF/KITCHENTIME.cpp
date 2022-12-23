cb81f3721fd6fa2575a6f905135bbddbd284aa47
Dartpixel <skartiksharma2004@gmail.com>
2022-12-23 22:07:32 +0530

    Updated

---
+++ CODECHEF/KITCHENTIME.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int x, y;
+        cin >> x >> y;
+        cout << y - x << endl;
+    }
+}
