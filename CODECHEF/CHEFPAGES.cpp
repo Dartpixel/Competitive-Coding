e4518846be2216d2f8aa6a50d8326d062e8e6fdd
Dartpixel <skartiksharma2004@gmail.com>
2022-12-28 22:43:05 +0530

    updated

---
+++ CODECHEF/CHEFPAGES.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cin >> a >> b;
+    if (a == 1 && b == 0)
+    {
+        cout << "https://www.codechef.com/contests" << endl;
+    }
+    else if (a == 0 && b == 1)
+    {
+        cout << "https://www.codechef.com/practice" << endl;
+    }
+    else if (a == 1 && b == 1)
+    {
+        cout << "https://discuss.codechef.com" << endl;
+    }
+    else
+    {
+        cout << "https://www.codechef.com/practice" << endl;
+    }
+    return 0;
+}
