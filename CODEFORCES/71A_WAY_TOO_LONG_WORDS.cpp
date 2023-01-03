7bc9c9c7f8d5cf60a142aff374dc3e1a76350e27
Dartpixel <skartiksharma2004@gmail.com>
2023-01-03 19:26:04 +0530

    updated

---
+++ CODEFORCES/71A_WAY_TOO_LONG_WORDS.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin>>testcase;
+    while(testcase--)
+    {
+        int c=0;
+        string str;
+        cin>>str;
+        int l=str.length();
+        for(int i=1; i<l-1; i++)
+        {
+            c++;
+        }
+        if(l>10)
+        {
+            cout<<str[0]<<c<<str[l-1]<<endl;
+        }
+        else
+        {
+            cout<<str<<endl;
+        }
+    }
+    return 0;
+}
