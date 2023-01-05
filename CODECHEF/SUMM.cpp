d98ce4e40f543063faba9089fc509eb44a35f6b3
Dartpixel <skartiksharma2004@gmail.com>
2023-01-05 13:40:12 +0530

    updated

---
+++ CODECHEF/SUMM.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int a, b, c;
+        cin >> a >> b >> c;
+        if (a + b == c)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
