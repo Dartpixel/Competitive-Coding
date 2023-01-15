c0bad98bd533dcce298a04cb16e00e49258264a0
Dartpixel <skartiksharma2004@gmail.com>
2023-01-15 15:54:34 +0530

    updated

---
+++ CODECHEF/LUDO.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int n;
+        cin >> n;
+        if (n == 6)
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
