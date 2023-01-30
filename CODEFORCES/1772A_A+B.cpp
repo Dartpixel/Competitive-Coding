15f1744f202fe842120952fd086519c9e120a289
Dartpixel <skartiksharma2004@gmail.com>
2023-01-30 10:26:28 +0530

    Updated

---
+++ CODEFORCES/1772A_A+B.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << a + b << endl;
+    }
+}
