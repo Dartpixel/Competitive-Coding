bf1c45981f30f7a6828f64a70c9538a136a2c42b
Dartpixel <skartiksharma2004@gmail.com>
2023-01-31 18:35:55 +0530

    Updated

---
+++ CODECHEF/BOTTOM.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int x;
+        cin >> x;
+        cout << 7 - x << endl;
+    }
+    return 0;
+}
