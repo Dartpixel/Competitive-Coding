575065c3fe5a855a6a3a073f363512cd1568fa11
Dartpixel <skartiksharma2004@gmail.com>
2023-02-06 23:19:17 +0530

    Updated

---
+++ LEETCODE/missing_number.cpp
@@ -0,0 +1,17 @@
+class Solution
+{
+public:
+    int missingNumber(vector<int> &nums)
+    {
+        int i;
+        sort(nums.begin(), nums.end());
+        for (i = 0; i < nums.size(); i++)
+        {
+            if (nums[i] != i)
+            {
+                break;
+            }
+        }
+        return i;
+    }
+};
