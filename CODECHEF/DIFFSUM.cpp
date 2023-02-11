0f10b4b1d758e335ab0f1c54b0a5e00cd6c43cf2
Dartpixel <skartiksharma2004@gmail.com>
2023-02-11 19:37:26 +0530

    Updated

---
+++ CODECHEF/DIFFSUM.cpp
@@ -0,0 +1,15 @@
+// stop stalking bruh 💀
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n1, n2;
+    cin >> n1 >> n2;
+    if (n1 > n2)
+        cout << n1 - n2;
+    else
+        cout << n1 + n2;
+    return 0;
+}
