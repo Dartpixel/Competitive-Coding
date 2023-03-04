c25218dd88619575eb7daccd9fbbd411cd6e68cd
Dartpixel <skartiksharma2004@gmail.com>
2023-03-05 00:13:04 +0530

    Updated

---
+++ CODEFORCES/1669A_DIVISION.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int n;
+        cin >> n;
+        if (n <= 1399)
+        {
+            cout << "Division 4" << endl;
+        }
+        else if (n >= 1400 && n <= 1599)
+        {
+            cout << "Division 3" << endl;
+        }
+        else if (n >= 1600 && n <= 1899)
+        {
+            cout << "Division 2" << endl;
+        }
+        else if (n >= 1900)
+        {
+            cout << "Division 1" << endl;
+        }
+    }
+    return 0;
+}
