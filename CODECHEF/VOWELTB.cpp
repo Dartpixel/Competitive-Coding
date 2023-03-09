60515ffba1bf33d2054c03631ee99f1912474f1a
Hardik Sharma <shardik2001@gmail.com>
2023-03-09 23:07:59 +0530

    Updated

---
+++ CODECHEF/VOWELTB.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char c;
+    cin >> c;
+    if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U')
+    {
+        cout << "Vowel" << endl;
+    }
+    else
+    {
+        cout << "Consonant" << endl;
+    }
+    return 0;
+}
