b7cdc90801d757a6dec81e6373b05ebb42f3670f
Hardik Sharma <shardik2001@gmail.com>
2023-03-11 20:28:27 +0530

    Updated

---
+++ LEETCODE/maximum_ice_cream_bars.cpp
@@ -0,0 +1,20 @@
+class Solution
+{
+public:
+    int maxIceCream(vector<int> &costs, int coins)
+    {
+        sort(begin(costs), end(costs));
+        for (int i = 0; i < costs.size(); i++)
+        {
+            if (coins >= costs[i])
+            {
+                coins = coins - costs[i];
+            }
+            else
+            {
+                return i;
+            }
+        }
+        return costs.size();
+    }
+};
