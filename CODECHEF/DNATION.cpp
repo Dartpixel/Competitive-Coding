797c718e5dc75f9e07646e00c3ca3384934da000
Kartik Sharma <skartiksharma2004@gmail.com>
2023-04-05 22:57:19 +0530

    Updated

---
+++ CODECHEF/DNATION.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int testcase;
+    cin >> testcase;
+    while (testcase--)
+    {
+        int a, b;
+        cin >> a >> b;
+        cout << b - a << endl;
+    }
+    return 0;
+}
