3dbbcc00bcd8877dfa4ec70b3d60c9c8bf6a7ff5
Kartik Sharma <skartiksharma2004@gmail.com>
2023-05-15 19:21:42 +0530

    Updated

--- CODECHEF/TENPACKETS.cpp
+++ CODECHEF/TENPACKETS.cpp
@@ -4,17 +4,17 @@ using namespace std;
 int main()
 {
 
-    int testcase;
-    cin >> testcase;
-    while (testcase--)
-    {
-        int x, y;
-        cin >> x >> y;
-        int p4 = 10 / 4;
-        int c4 = p4 * 4;
-        int c2 = 10 - c4;
-        int p2 = c2 / 2;
-        cout << p2 * x + p4 * y << endl;
-    }
-    return 0;
+	int testcase;
+	cin >> testcase;
+	while (testcase--)
+	{
+		int x, y;
+		cin >> x >> y;
+		int p4 = 10 / 4;
+		int c4 = p4 * 4;
+		int c2 = 10 - c4;
+		int p2 = c2 / 2;
+		cout << p2 * x + p4 * y << endl;
+	}
+	return 0;
 }
