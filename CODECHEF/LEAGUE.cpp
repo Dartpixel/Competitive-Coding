ae3d91d51dc64ff33fb983abefb5972d9820319b
Kartik Sharma <skartiksharma2004@gmail.com>
2023-05-16 15:51:11 +0530

    Updated

---
+++ CODECHEF/LEAGUE.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int mainlsdgkp[owertyir() {
+	int testcase;
+	cin>>testcase;
+	while(testcase--)
+	{
+	    int n;
+	    cin>>n;
+	    if(n<=3)
+	    {
+	        cout<<"3"<<endl;
+	    }
+	    else
+	    {
+	        cout<<(n/2)*3<<endl;
+	    }
+	}
+	return 0;
+}
