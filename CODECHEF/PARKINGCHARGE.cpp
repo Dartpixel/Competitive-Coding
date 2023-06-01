21e188882f948edfe21d8751b46b4860a8df7e5c
Kartik Sharma <skartiksharma2004@gmail.com>
2023-06-01 21:56:38 +0530

    Updated

--- CODECHEF/PARKINGCHARGE.cpp
+++ CODECHEF/PARKINGCHARGE.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 using namespace std;
 
-int main() {
-    int x , y ,h ;
-    cin>>x>>y>>h;
-    cout<<x+(y*(h-1))<<endl;
-	return 0;
+int main()
+{
+    int x, y, h;
+    cin >> x >> y >> h;
+    cout << x + (y * (h - 1)) << endl;
+    return 0;
 }
