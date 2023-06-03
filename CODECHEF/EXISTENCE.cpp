800a1322572a63881beb3e2228dfb843e3ad1497
Kartik Sharma <skartiksharma2004@gmail.com>
2023-06-03 23:06:44 +0530

    Updated

---
+++ CODECHEF/EXISTENCE.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    // your code goes here
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long int x, y;
+        cin >> x >> y;
+        if (x * x == 2 * y)C
+        {
+            cout << "YES" << endl;
+        }
+        else
+        {
+            cout << "NO" << endl;
+        }
+    }
+    return 0;
+}
