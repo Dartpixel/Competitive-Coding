cd381f29cc3698905989ce0d8c4aa486098ccef2
Kartik Sharma <skartiksharma2004@gmail.com>
2023-07-26 21:26:03 +0530

    Updated

--- CODECHEF/CHFSPL.cpp
+++ CODECHEF/CHFSPL.cpp
@@ -1,19 +1,23 @@
 #include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
 using namespace std;
 
 int main()
 {
-    int testcase;
-    cin >> testcase;
-    while (testcase--)
+    int t;
+    cin >> t;
+    while (t--)
     {
-        int a, b, c;
-        cin >> a >> b >> c;
-        int x = a + b;
-        int y = b + c;
-        int z = a + c;
-        int ans = max(x, y);
-        cout << max(ans, z) << endl;
+        vector<int> ans;
+        for (int i = 0; i < 3; i++)
+        {
+            int n;
+            cin >> n;
+            ans.push_back(n);
+        }
+        sort(ans.rbegin(), ans.rend());
+        cout << ans[0] + ans[1] << endl;
     }
     return 0;
 }
