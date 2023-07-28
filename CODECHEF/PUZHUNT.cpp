2fda750bfda6ad63c183c4a102b4304eaea9fa66
Kartik Sharma <skartiksharma2004@gmail.com>
2023-07-28 21:18:57 +0530

    Updated

---
+++ CODECHEF/PUZHUNT.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int N;
+    cin >> N;
+    if (N >= 6 && N <= 8)
+    {
+        cout << "Yes" << endl;
+    }
+    else
+    {
+        cout << "No" << endl;
+    }
+
+    return 0;
+}
