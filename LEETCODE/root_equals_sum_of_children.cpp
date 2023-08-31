f2b4e895b712c8a1d655065d7681154523fa4b43
Kartik Sharma <skartiksharma2004@gmail.com>
2023-08-31 22:55:02 +0530

    Updated

---
+++ LEETCODE/root_equals_sum_of_children.cpp
@@ -0,0 +1,26 @@
+//********************* Creator- itwasme *******************//
+//******************* Created on- 15/02/23 *******************//
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    bool checkTree(TreeNode *root)
+    {
+        if (root->val == ((root->left->val) + (root->right->val)))
+        {
+            return true;
+        }
+        return false;
+    }
+};
