cafa3dd3a0583b4f5bb0b4641a8cc33990ad81d3
Kartik Sharma <skartiksharma2004@gmail.com>
2023-09-11 22:38:56 +0530

    Updated

---
+++ LEETCODE/concatination_of_array.cpp
@@ -0,0 +1,16 @@
+//********************* Creator- itwasme *******************//
+//******************** Created on- 09/02/23 *******************//
+
+class Solution
+{
+public:
+    vector<int> getConcatenation(vector<int> &nums)
+    {
+        int n = nums.size();
+        for (int i = 0; i < n; i++)
+        {
+            nums.push_back(nums[i]);
+        }
+        return nums;
+    }
+};
