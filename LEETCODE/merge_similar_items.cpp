89cb8eb5e3e6c2a8af0cd5ed3d296690af1317a8
Kartik Sharma <skartiksharma2004@gmail.com>
2023-11-17 19:37:20 +0530

    Updated

---
+++ LEETCODE/merge_similar_items.cpp
@@ -0,0 +1,21 @@
+class Solution
+{
+public:
+    vector<vector<int>> mergeSimilarItems(vector<vector<int>> &items1, vector<vector<int>> &items2)
+    {
+
+        map<int, int> m;
+        int n1 = items1.size();
+        for (auto i = 0; i < n1; i++)
+            m[items1[i][0]] = items1[i][1];
+
+        int n2 = items2.size();
+        for (int j = 0; j < n2; j++)
+            m[items2[j][0]] += items2[j][1];
+
+        vector<vector<int>> ans;
+        for (auto it : m)
+            ans.push_back({it.first, it.second});
+        return ans;
+    }
+};
