1e706c44776316949d44792f4ba90770ab54edce
Kartik Sharma <skartiksharma2004@gmail.com>
2023-12-09 23:36:13 +0530

    Updated

---
+++ LEETCODE/binary_tree_postorder_traversal.cpp
@@ -0,0 +1,39 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution
+{
+public:
+    vector<int> ans;
+
+    void postorder(TreeNode *root)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+        if (root->left)
+        {
+            postorder(root->left);
+        }
+        if (root->right)
+        {
+            postorder(root->right);
+        }
+        ans.push_back(root->val);
+    }
+
+    vector<int> postorderTraversal(TreeNode *root)
+    {
+        postorder(root);
+        return ans;
+    }
+};
