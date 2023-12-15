74c44434de0b7cfba4380b9d654bb70d35ced307
Kartik Sharma <skartiksharma2004@gmail.com>
2023-12-15 23:33:52 +0530

    Updated

---
+++ LEETCODE/goal_parser_interpretation.cpp
@@ -0,0 +1,18 @@
+class Solution
+{
+public:
+    string interpret(string command)
+    {
+        string res = "";
+        for (int i = 0; i < command.size(); i++)
+        {
+            auto curr = command[i];
+            if (i && curr == ')' && command[i - 1] == '(')
+                res += 'o';
+            if (curr == '(' || curr == ')')
+                continue;
+            res += curr;
+        }
+        return res;
+    }
+};
